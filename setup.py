6546ed9765793054da8ece8977602e760a7da857
Francois Wautier <francois@wautier.eu>
2019-03-18 19:54:09 +0700

    Changed to setuptools

--- setup.py
+++ setup.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import setuptools
+version = "0.1.0b2"
 
-from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-version = "0.1.0b1"
-
-setup(name='aiotuya',
-      packages=['aiotuya'],
-      version=version,
-      author='François Wautier, Max Isom et al.',
-      author_email='francois@wautier.eu',
-      description='Pure Python library to control Tuya devices',
-      url='https://github.com/frawau/aiotuya',
-      download_url='https://github.com/frawau/aiotuya/archive/'+version+'.tar.gz',
-      platforms=['unix', 'linux', 'osx'],
-      license='LGPL',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: MIT LicenseO',
-          'Operating System :: POSIX',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: MacOS :: MacOS X',
-          'Topic :: Software Development :: Libraries',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: CPython'
-      ],
-      keywords=[
-          'Tuya', 'IoT', 'WiFi', 'Home Automation',  'asyncio',
-      ],
-      install_requires=[
-          'colorsys', 'csv'
-      ],
-      entry_points={
-          'console_scripts': [
-              'aiotuya=aiotuya.__main__:main'
-          ],
-      },
-      )
+setuptools.setup(name='aiotuya',
+    packages=['aiotuya'],
+    version=version,
+    author='François Wautier, Max Isom et al.',
+    author_email='francois@wautier.eu',
+    description='Pure Python library to control/provision Tuya devices',
+    long_description=long_description,
+    url='https://github.com/frawau/aiotuya',
+    platforms=['unix', 'linux', 'osx'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT LicenseO',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    keywords=[
+        'Tuya', 'IoT', 'WiFi', 'Home Automation',  'asyncio',
+    ],
+    install_requires=[
+        'colorsys', 'csv'
+    ],
+    entry_points={
+        'console_scripts': [
+            'aiotuya=aiotuya.__main__:main'
+        ],
+    },
+    zip_safe=False
+    )
